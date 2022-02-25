d5cdce82e5c3376e1f96a6b0e7c56f536e36e5ec
A21SilvanaAF <A21SilvanaAF@a13eq18.sanclemente.local>
2022-02-25 17:31:01 +0100

    Análise con SonarLint e modificacións acordes á información obtida

--- src/test/java/com/mycompany/proxectoparidade/ParMainTest.java
+++ src/test/java/com/mycompany/proxectoparidade/ParMainTest.java
@@ -1,6 +1,5 @@
 package com.mycompany.proxectoparidade;
 
-import com.mycompany.proxectoparidade.ParMain;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Disabled;
